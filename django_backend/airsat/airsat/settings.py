f0952e5b99c4e3fb3fca76d7b40853457a89f833
Dhaneswari Behera <dhanimsclrl@gmail.com>
2026-05-12 11:07:24 +0530

    Added pollution app

--- django_backend/airsat/airsat/settings.py
+++ django_backend/airsat/airsat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-6h7!bt2xr_yum@l9d6v94#qas+*oousj3mkxgvml9pqnm$2x(5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "pollution",
 ]
 
 MIDDLEWARE = [
